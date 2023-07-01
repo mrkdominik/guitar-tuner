2ecdba48c196b8a249e77574c0869b90a19edad0
sjnyth <saujanya99@outlook.com>
2023-07-01 13:02:26 -0400

    dft completed

--- DFT_tuner.py
+++ DFT_tuner.py
@@ -1,5 +1,6 @@
-import numpy as np 
 import sounddevice as sd 
+import numpy as np 
+import scipy
 import scipy.fftpack
 import os
 
@@ -28,20 +29,39 @@ def find_closest_note(pitch):
 # The sounddevice callback function
 # Provides us with new data once window_step samples have been finalized
 
-def callback(indata, frames, times, status):
+def callback(indata, frames, time, status):
     global windowSamples
     if status: 
         print(status)
 
     if any(indata):
-        windowSamples = np.concatenate((windowSamples, indata[:0])) # append new samples
-        windowSamples = windowSamples[len(indata[:,0])] # remove old samples
-        maginitudeSpec = abs(scipy.fftpack.fft(windowSamples)[:len(windowSamples)//2])
+        windowSamples = np.concatenate((windowSamples, indata[:,0])) # append new samples
+        windowSamples = windowSamples[len(indata[:,0]):] # remove old samples
+        magnitudeSpec = abs(scipy.fftpack.fft(windowSamples)[:len(windowSamples)//2])
 
         for i in range(int(62/(sample_freq/window_size))):
-            maginitudeSpec[i] = 0
+            magnitudeSpec[i] = 0
 
-        maxInd = np.argmax(maginitudeSpec)
+        maxInd = np.argmax(magnitudeSpec)
         maxFreq = maxInd * (sample_freq/window_size)
         closestNote, closestPitch = find_closest_note(maxFreq)
-        
+
+        if os.name == 'nt':
+                os.system('cls')
+        else:
+            os.system('clear')
+
+        print(f"Closest note: {closestNote} {maxFreq:.1f}/{closestPitch:.1f}")
+    
+    else: 
+        print("no input")
+
+try:
+    with sd.InputStream(channels = 1, callback = callback, 
+         blocksize = window_step, samplerate = sample_freq):
+         
+         while True: 
+            pass
+
+except Exception as e: 
+    print(str(e))
